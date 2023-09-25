81d2210761b27d6824f26c1054ac0ccb7e53f27f
PGHOON <park1578@purdue.edu>
2023-09-26 02:01:27 +0900

    receiving image file(Error)

--- Server/serverside.py
+++ Server/serverside.py
@@ -8,11 +8,9 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.bind((HOST, PORT))
 
-# Listen for incoming connections
 server_socket.listen(1)
 print("Waiting for a connection...")
 
-# Accept a connection
 client_socket, client_address = server_socket.accept()
 print(f"Connection from {client_address}")
 
@@ -23,14 +21,25 @@ while True:
             print("Connection closed by client.")
             break
 
-        # Connection Cheking
         if data == '...':
             print("...")
+        elif data == ':right':
+            print("right button pressed")
+            file = open('copymelon.jpg', "wb")
+            jpg_chunk = client_socket.recv(2048)
+
+            while jpg_chunk:    ## I guess this while-loop never ends...
+                file.write(jpg_chunk)
+                jpg_chunk = client_socket.recv(2048)
+                if not jpg_chunk:
+                    file.close()
+                    continue
+            print("Image received")
 
     except ConnectionResetError:
         print("Connection closed by client.")
         break
-    
+
+file.close()
 client_socket.close()
 server_socket.close()
-
