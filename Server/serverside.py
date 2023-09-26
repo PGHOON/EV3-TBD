ff2663335b331a7e4fba5cf92f6eedcedca54728
PGHOON <park1578@purdue.edu>
2023-09-26 09:04:49 +0900

    predicting fruit type sent by EV3

--- Server/serverside.py
+++ Server/serverside.py
@@ -1,4 +1,8 @@
 import socket
+import numpy as np
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+from PIL import Image
 
 HOST = '0.0.0.0'    #or Replace "0.0.0.0" with the IP address that appears in the top left corner of the EV3 brick's screen.
                     #0.0.0.0 will listen every port available
@@ -36,6 +40,22 @@ while True:
                     file.write(jpg_chunk)
                     received_size += len(jpg_chunk)
             print("Image received and saved as 'received_image.jpg'")
+
+
+            ### predicting the Image ###
+            model = load_model('model.h5')      #Train accuracy : 97%, Test accuracy : 45%
+            image_path = 'received_image.jpg'   #A pretty decent model, considering that the class number is 10.
+            img = Image.open(image_path)
+            img = img.resize((32, 32))
+            img = np.array(img)
+            img = np.expand_dims(img, axis=0)
+            img = preprocess_input(img)
+
+            predictions = model.predict(img)
+
+            predicted_class = np.argmax(predictions, axis=1)
+
+            print("Predicted class:", predicted_class)
 
     except ConnectionResetError:
         print("Connection closed by client.")
